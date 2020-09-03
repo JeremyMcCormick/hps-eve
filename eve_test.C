b627b68d5c7e59fccc3651cbecc344df7a949771
Jeremy McCormick <jermccormick@gmail.com>
2020-09-02 22:29:36 -0500

    Add ecal and make method to add geo nodes generic.

--- eve_test.C
+++ eve_test.C
@@ -10,24 +10,20 @@
 #include "TGeoNavigator.h"
 #include "TEveTrans.h"
 
-TEveElementList* build_tracker(TGeoManager* geo) {
-    auto trackerElements = new TEveElementList("Tracker");
-    //TGeoNode* trackingVol = geo->GetTopVolume()->FindNode("tracking_volume_0");
-    //TGeoNode* baseVol = trackingVol->GetVolume()->FindNode("base_volume_0");
-    geo->cd("/world_volume_1/tracking_volume_0/base_volume_0");
+// Add elements which are all children of a single volume
+void addGeoElements(TGeoManager* geo,
+                    TEveManager* mgr,
+                    const char* name,
+                    const char* path,
+                    const char* patt) {
+    auto elements = new TEveElementList(name);
+    geo->cd(path);
     auto ndau = geo->GetCurrentNode()->GetNdaughters();
     for (int i=0; i<ndau; i++) {
         geo->CdDown(i);
-        //TGeoNode* node = baseVol->GetDaughter(i);
         TGeoNode* node = geo->GetCurrentNode();
-        std::cout << "cd into: " << geo->GetCurrentNode() << std::endl;
-        if (std::string(node->GetName()).find("module_L") != std::string::npos) {
-            std::cout << "Adding module: " << node->GetName() << std::endl;
+        if (std::string(node->GetName()).find(patt) != std::string::npos) {
             auto nodeName = node->GetName();
-            std::string path = std::string("/world_volume_1/tracking_volume_0/base_volume_0/") + std::string(nodeName);
-
-            geo->cd(path.c_str());
-
             TGeoVolume* vol = gGeoManager->GetCurrentVolume();
             TEveGeoShape* shape = new TEveGeoShape(node->GetName(), vol->GetMaterial()->GetName());
             shape->SetShape((TGeoShape*) vol->GetShape()->Clone());
@@ -35,11 +31,11 @@ TEveElementList* build_tracker(TGeoManager* geo) {
             shape->SetFillColor(vol->GetFillColor());
             shape->SetMainTransparency(vol->GetTransparency());
             shape->RefMainTrans().SetFrom(*geo->GetCurrentMatrix());
-            trackerElements->AddElement(shape);
+            elements->AddElement(shape);
         }
         geo->CdUp();
     }
-    return trackerElements;
+    mgr->AddGlobalElement(elements);
 }
 
 void eve_test() {
@@ -62,12 +58,17 @@ void eve_test() {
 
     auto topNode = gGeoManager->GetTopNode();
 
-    //auto eveNode = new TEveGeoTopNode(geo, topNode);
-    //eveNode->ExpandIntoListTreesRecursively();
-    //manager->AddGlobalElement(eveNode);
+    addGeoElements(geo,
+                   manager,
+                   "SVT",
+                   "/world_volume_1/tracking_volume_0/base_volume_0",
+                   "module_L");
 
-    TEveElement* tracker = build_tracker(geo);
-    manager->AddGlobalElement(tracker);
+    addGeoElements(geo,
+                   manager,
+                   "ECAL",
+                   "/world_volume_1",
+                   "crystal_volume");
 
     browser->SetTabTitle("Event Control", 0);
     browser->StopEmbedding();
