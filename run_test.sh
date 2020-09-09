1245e6a88383044074b7ffba6a11048c2323472a
Jeremy McCormick <jermccormick@gmail.com>
2020-09-09 15:59:01 -0500

    Run with verbose 0 in test script.

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,4 @@
 
 event_file=/work/slac/data/lcio/hpsForward_e+_0.5GeV_z-7.5_0_SLIC-v06-00-01_QGSP_BERT_HPS-PhysicsRun2019-v2-4pt5_recon.slcio
 
-./install/bin/hps-eve -g ./HPS-PhysicsRun2019-v2-4pt5.gdml -v 1 -b 1.034 -e HodoscopeHits -e TrackerHitsECal -e HodoscopePreprocessedHits $event_file
+./install/bin/hps-eve -g ./HPS-PhysicsRun2019-v2-4pt5.gdml -v 0 -b 1.034 -e HodoscopeHits -e TrackerHitsECal -e HodoscopePreprocessedHits $event_file
