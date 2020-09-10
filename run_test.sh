f51817c2e368f82bd6808f1ef2512d6894701344
Jeremy McCormick <jermccormick@gmail.com>
2020-09-09 22:55:31 -0500

    Test file caching in test script.

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,7 @@
 
 event_file=/work/slac/data/lcio/hpsForward_e+_0.5GeV_z-7.5_0_SLIC-v06-00-01_QGSP_BERT_HPS-PhysicsRun2019-v2-4pt5_recon.slcio
 
-./install/bin/hps-eve -g ./HPS-PhysicsRun2019-v2-4pt5.gdml -v 0 -b 1.034 -e HodoscopeHits -e TrackerHitsECal -e HodoscopePreprocessedHits $event_file
+#./install/bin/hps-eve -g ./HPS-PhysicsRun2019-v2-4pt5.gdml -v 0 -b 1.034 -e HodoscopeHits -e TrackerHitsECal -e HodoscopePreprocessedHits $event_file
+
+# When GDML file is not provided, an LCDD file will be downloaded and GDML extracted from it.
+./install/bin/hps-eve -v 4 -b 1.034 -e HodoscopeHits -e TrackerHitsECal -e HodoscopePreprocessedHits $event_file
